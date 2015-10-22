f5388f1658ca4280d2f59bc933d87d3af9fbc2d0
John Rae <john.rae23@gmail.com>
2015-10-22 17:56:00 -0400

    initial commit, also i did a bunch oops

---
+++ js/tests.js
@@ -0,0 +1,41 @@
+import GoodGuy from './goodguy';
+
+(function() {
+  'use strict';
+
+  describe('About My Good Guy', function() {
+
+    beforeEach( function() {
+      window.x = new GoodGuy();
+    });
+
+    describe('creating a good guy', function() {
+      it('should create an instance of Good Guy', function () {
+        expect(x instanceof GoodGuy).to.equal(true);
+      });
+    });
+
+    describe('my player health', function() {
+      it('should have full health upon creation', function() {
+        expect(x.health).to.equal(300);
+      });
+    });
+
+    describe('player taking damage', function() {
+      it('should lower the score, after getting hit', function() {
+        x.hit();
+        expect(x.health).to.be.below(300);
+      })
+    });
+
+  });
+
+}());
+
+//good guy
+
+//bad guy
+
+//both have attack methods
+
+//both will have health
